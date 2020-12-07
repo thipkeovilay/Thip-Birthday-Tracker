60a9d5fd1b8c432987429b9dd8344a39229371c2
Thip <thip.keovilay@gmail.com>
2020-12-07 16:43:33 -0500

    Created directories, configured a connection to MongoDB, created a model and schema for birthday objects.

---
+++ server/models/birthday.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose'),
+  validator = require('validator'),
+  bcrypt = require('bcryptjs'),
+  jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  cohort_number: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  month: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
